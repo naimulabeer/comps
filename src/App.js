cb6aed14c110379948539007bae3f5420f92b927
naimulabeer <naimul.h.abeer@gmail.com>
2023-05-29 15:03:16 +0600

    underlying elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import Button from "./Button";
+
 function App() {
-  return <div>Components App</div>;
+  return (
+    <div>
+      <div>
+        <Button></Button>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,5 @@
+function Button() {
+  return <button>Hello World!</button>;
+}
+
+export default Button;
