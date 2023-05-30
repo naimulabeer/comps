ef582450d88e7bc2653a7c385f7868c2be717798
naimulabeer <naimul.h.abeer@gmail.com>
2023-05-30 22:13:31 +0600

    conditional icon rendering

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,12 +2,22 @@ import { useState } from "react";
 
 function Accordion({ items }) {
   const [expandedItems, setExpandedItems] = useState(0);
+
+  const handleClick = (nextIndex) => {
+    setExpandedItems(nextIndex);
+  };
+
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedItems;
 
+    const icon = <span>{isExpanded ? "DOWN" : "LEFT"}</span>;
+
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
+        <div onClick={() => handleClick(index)}>
+          {icon}
+          {item.label}
+        </div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
