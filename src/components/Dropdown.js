f0e0b0b559cd0e104cfffffb0202fcaeaed6c7e8
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-04 17:28:36 +0600

    checking click location

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -8,8 +8,15 @@ function Dropdown({ options, value, onChange }) {
 
   useEffect(() => {
     const handler = (event) => {
-      console.log(divEl.current);
+      if (!divEl.current) {
+        return;
+      }
+
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      }
     };
+
     document.addEventListener("click", handler, true);
 
     return () => {
