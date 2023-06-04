ad173a2f51790e17addb52e496c4b315cb8d0305
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-04 21:01:45 +0600

    extracting the DropdownPage

--- src/pages/DropdownPage.js
+++ src/pages/DropdownPage.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import Dropdown from "./components/Dropdown";
+import Dropdown from "../components/Dropdown";
 
-function App() {
+function DropdownPage() {
   const [selection, setSelection] = useState(null);
 
   const handleSelect = (option) => {
@@ -15,11 +15,9 @@ function App() {
   ];
   return (
     <div className="flex">
-      <a href="/dashboard">Dashboard</a>
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
       <Dropdown options={options} value={selection} onChange={handleSelect} />
     </div>
   );
 }
 
-export default App;
+export default DropdownPage;
