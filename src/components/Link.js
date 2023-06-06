5941509ce52dbaebb1464c5fff6f765b507f9a68
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-07 00:19:09 +0600

    bit of styling

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,11 @@
+import classNames from "classnames";
 import { useContext } from "react";
 import NavigationContext from "../context/navigation";
 
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
+
+  const classes = classNames("text-blue-500");
 
   const handleClick = (event) => {
     if (event.metaKey || event.ctrlKey) {
@@ -13,7 +16,7 @@ function Link({ to, children }) {
     navigate(to);
   };
   return (
-    <a href={to} onClick={handleClick}>
+    <a className={classes} href={to} onClick={handleClick}>
       {children}
     </a>
   );
