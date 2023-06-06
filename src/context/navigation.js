cb4dc8c32980129939b08d896ac867207cd8e759
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-06 16:01:42 +0600

    navigation context

---
+++ src/context/navigation.js
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+const NavigationContext = createContext();
+
+function NavigationProvider({ children }) {
+  return (
+    <NavigationContext.Provider value={{}}>
+      {children}
+    </NavigationContext.Provider>
+  );
+}
+
+export { NavigationProvider };
+
+export default NavigationContext;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import "./index.css";
 import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App";
+import { NavigationProvider } from "./context/Navigation";
 
 const el = document.getElementById("root");
 const root = ReactDom.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
+);
