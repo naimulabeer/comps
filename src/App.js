c2e3c10b4de4bbd6ca22c78c42e5515431b8b816
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-06 16:57:25 +0600

    Link component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Link from "./components/Link";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/accordion">Go to Accordion</Link>
+      <Link to="/dropdown">Go to Dropdown</Link>
+    </div>
+  );
 }
 
 export default App;
