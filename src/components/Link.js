cb92eb2964617dc0d0d726171367f9fdb4619c44
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-06 23:52:56 +0600

    handling control and command keys

--- src/components/Link.js
+++ src/components/Link.js
@@ -5,11 +5,18 @@ function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
 
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
 
     navigate(to);
   };
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
