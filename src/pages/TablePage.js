f88fb9cc5d2089ed752221442e01644e03911e6a
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-07 20:46:35 +0600

    fixing the colors

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -10,7 +10,10 @@ function TablePage() {
 
   const config = [
     { label: "Name", render: (fruit) => fruit.name },
-    { label: "Color", render: (fruit) => fruit.color },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
     { label: "Score", render: (fruit) => fruit.score },
   ];
   return (
