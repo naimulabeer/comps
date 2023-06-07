4d8197b7d48ad8a63dd023b0f388ea78abdd54dc
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-08 03:00:03 +0600

    table wrapup

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,3 @@
-//import Table from "../components/Table";
 import SortableTable from "../components/SortableTable";
 
 function TablePage() {
@@ -7,6 +6,7 @@ function TablePage() {
     { name: "Apple", color: "bg-red-500", score: 3 },
     { name: "Banana", color: "bg-yellow-500", score: 1 },
     { name: "Lime", color: "bg-green-500", score: 4 },
+    { name: "Cherry", color: "bg-red-700", score: 2.5 },
   ];
 
   const config = [
@@ -23,6 +23,11 @@ function TablePage() {
       label: "Score",
       render: (fruit) => fruit.score,
       sortValue: (fruit) => fruit.score,
+    },
+    {
+      label: "Score Squared",
+      render: (fruit) => fruit.score ** 2,
+      sortValue: (fruit) => fruit.score ** 2,
     },
   ];
 
