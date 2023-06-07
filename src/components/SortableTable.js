f17eaa103f3536b95f4783edd35179aecff461df
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-08 02:54:38 +0600

    resetting sorted order

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -8,6 +8,12 @@ function SortableTable(props) {
   const { config, data } = props;
 
   const handleClick = (label) => {
+    if (sortBy && label !== sortBy) {
+      setSortOrder("asc");
+      setSortBy(label);
+      return;
+    }
+
     if (sortOrder === null) {
       setSortOrder("asc");
       setSortBy(label);
@@ -57,12 +63,7 @@ function SortableTable(props) {
     });
   }
 
-  return (
-    <div>
-      {sortOrder} - {sortBy}
-      <Table {...props} data={sortedData} config={updatedConfig} />
-    </div>
-  );
+  return <Table {...props} data={sortedData} config={updatedConfig} />;
 }
 
 function getIcons(label, sortBy, sortOrder) {
