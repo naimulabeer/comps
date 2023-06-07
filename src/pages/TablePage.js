75492ceba03ae60cea681ab4af442e7947dd5c90
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-07 20:52:04 +0600

    adding a key function

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -16,9 +16,13 @@ function TablePage() {
     },
     { label: "Score", render: (fruit) => fruit.score },
   ];
+
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
   return (
     <div>
-      <Table data={data} config={config} />
+      <Table data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
