f9b90547eb976db886c662c0e7151c2ae5c214a0
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-08 01:44:06 +0600

    adding sort state

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,10 +1,22 @@
 import Table from "./Table";
+import { useState } from "react";
 
 function SortableTable(props) {
   const { config } = props;
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
 
   const handleClick = (label) => {
-    console.log(label);
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+      setSortBy(null);
+    }
   };
 
   const updatedConfig = config.map((column) => {
@@ -21,7 +33,12 @@ function SortableTable(props) {
     };
   });
 
-  return <Table {...props} config={updatedConfig} />;
+  return (
+    <div>
+      {sortOrder} - {sortBy}
+      <Table {...props} config={updatedConfig} />
+    </div>
+  );
 }
 
 export default SortableTable;
