ea461b0122952d8f605c5f8c878e95160c964ab2
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-09 03:31:00 +0600

    adding the form (reducer)

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import Button from "../components/Button";
-import useCounter from "../hooks/use-counter";
+import Panel from "../components/Panel";
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div>
-      <h1>Count is {count}</h1>
-      <Button onClick={increment}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <div className="flex flex-row">
+        <h1 className="text-lg">Count is {count}</h1>
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form>
+        <label>Add a lot!</label>
+        <input
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button>Add It!</Button>
+      </form>
+    </Panel>
   );
 }
 
