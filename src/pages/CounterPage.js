2d9917ec4a71a30db6700c7ee2fba71a113d4fae
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-09 03:39:40 +0600

    more on the form

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -5,12 +5,27 @@ import Panel from "../components/Panel";
 function CounterPage({ initialCount }) {
   const [count, setCount] = useState(initialCount);
 
+  const [valueToAdd, setValueToAdd] = useState(0);
+
   const increment = () => {
     setCount(count + 1);
   };
 
   const decrement = () => {
     setCount(count - 1);
+  };
+
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0;
+
+    setValueToAdd(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setCount(count + valueToAdd);
+    setValueToAdd(0);
   };
 
   return (
@@ -21,9 +36,11 @@ function CounterPage({ initialCount }) {
         <Button onClick={decrement}>Decrement</Button>
       </div>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
+          value={valueToAdd || ""}
+          onChange={handleChange}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
         />
