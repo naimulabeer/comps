f09c12437e76d639eafd8d48d3df57c1243c790c
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-10 19:39:14 +0600

    useReducer in action

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,37 +1,43 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
 function CounterPage({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
+  const reducer = (state, action) => {};
+  // const [count, setCount] = useState(initialCount);
 
-  const [valueToAdd, setValueToAdd] = useState(0);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
 
   const increment = () => {
-    setCount(count + 1);
+    // setCount(count + 1);
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    // setCount(count - 1);
   };
 
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
 
-    setValueToAdd(value);
+    // setValueToAdd(value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
   };
 
   return (
     <Panel className="m-3">
       <div className="flex flex-row">
-        <h1 className="text-lg">Count is {count}</h1>
+        <h1 className="text-lg">Count is {state.count}</h1>
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Decrement</Button>
       </div>
@@ -39,7 +45,7 @@ function CounterPage({ initialCount }) {
       <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
-          value={valueToAdd || ""}
+          value={state.valueToAdd || ""}
           onChange={handleChange}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
